8a3bc700be66ef70157dc6db78aa522ced85cb0b
徐绍平 <xusp@yunxi.tv>
2019-01-03 21:15:53 +0800

    up

---
+++ src/views/index/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="page-index">
+  
+    <div id="app">
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: "page-index",
+  data() {
+    return {};
+  },
+  filters: {},
+  methods: {},
+  components: {},
+  mounted: function() {}
+};
+</script>
+
+<style lang="less">
+.page-index {
+}
+</style>
---
+++ src/views/other/other.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="page-other">
+  other
+  
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: "page-other",
+  data() {
+    return {};
+  },
+  filters: {},
+  methods: {},
+  components: {},
+  mounted: function() {}
+};
+</script>
+
+<style lang="less">
+.page-other {
+}
+</style>
